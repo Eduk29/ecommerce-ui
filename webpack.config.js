04cd5274ea5bdf9d0ed20d901e538aa3c23119d5
Eduardo Marques <edu.temarques@gmail.com>
2019-10-27 20:03:00 -0300

    feat: Adjust in webpack css loader

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2", // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: "url-loader?name=[name].[ext]",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  externals: {
+    react: "commonjs react", // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+  },
+  resolve: {
+    alias: {
+      "@utils": path.resolve(__dirname, "src", "utils"),
+    },
+  },
+}
