8417a05a2f6b9c5521e2aa427a277c8486ee9ca5
Eduk29 <edu.temarques@gmail.com>
2019-11-15 15:51:52 -0300

    feat: Add UiMenu in Navbar component

--- src/components/UiNavbar/UiNavbar.js
+++ src/components/UiNavbar/UiNavbar.js
@@ -4,6 +4,7 @@ import React from "react"
 // Material
 import {
   AppBar,
+  Button,
   IconButton,
   makeStyles,
   Toolbar,
@@ -11,9 +12,17 @@ import {
 } from "@material-ui/core"
 import { AccountCircle, Menu } from "@material-ui/icons"
 
+// Components
+import { UiMenu } from "../UiMenu"
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+  },
+  button: {
+    color: 'white',
+    margin: theme.spacing(2),
+    fontWeight: 'bold'
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -23,35 +32,102 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
-export const UiNavbar = ({ hasLoginButton, hasMenuButton, label }) => {
+const loginMenuItemList = [
+  {
+    id: 1,
+    label: "Profile",
+    handleClick: () => {
+      console.log("Redirect to Profile Page")
+    },
+  },
+  {
+    id: 2,
+    label: "Logout",
+    handleClick: () => {
+      console.log("Logout and redirect to Home Page")
+    },
+  },
+]
+
+export const UiNavbar = ({
+  authenticatedUser,
+  handleClickLoginButton,
+  hasLoginButton,
+  hasMenuButton,
+  label,
+  menuItemList
+}) => {
+  const [menuAnchorEl, setMenuAnchorEl] = React.useState(null)
+  const [loginAnchorEl, setLoginAnchorEl] = React.useState(null)
   const classes = useStyles()
+
+  const handleOpenMenu = event => {
+    setMenuAnchorEl(event.currentTarget)
+  }
+
+  const handleCloseMenu = () => {
+    setMenuAnchorEl(null)
+  }
+
+  const handleOpenLoginMenu = event => {
+    setLoginAnchorEl(event.currentTarget)
+  }
+
+  const handleCloseLoginMenu = () => {
+    setLoginAnchorEl(null)
+  }
+
+  const displayMenuButton = () => {
+    return hasMenuButton && menuItemList.length > 0
+  }
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          {hasMenuButton && (
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <Menu />
-            </IconButton>
+          { displayMenuButton() && (
+            <div>
+              <IconButton
+                aria-label="menu"
+                className={classes.menuButton}
+                color="inherit"
+                edge="start"
+                onClick={handleOpenMenu}
+              >
+                <Menu />
+              </IconButton>
+              <UiMenu
+                anchorEl={menuAnchorEl}
+                handleClose={handleCloseMenu}
+                menuItemList={menuItemList}
+              />
+            </div>
           )}
           <Typography variant="h5" className={classes.title}>
             {label}
           </Typography>
-          {hasLoginButton && (
-            <IconButton
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
+          {hasLoginButton && authenticatedUser && (
+            <div>
+              <IconButton
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                aria-label="account of current user"
+                color="inherit"
+                onClick={handleOpenLoginMenu}
+              >
+                <AccountCircle />
+              </IconButton>
+              <UiMenu
+                anchorEl={loginAnchorEl}
+                handleClose={handleCloseLoginMenu}
+                menuItemList={loginMenuItemList}
+              />
+            </div>
+          )}
+          {hasLoginButton && !authenticatedUser && (
+            <Button className={classes.button} onClick={handleClickLoginButton}>
+              Login
+            </Button>
           )}
         </Toolbar>
       </AppBar>
