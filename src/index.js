fb243a074fff44dc46d3d9e114df51c78524f2f8
Eduardo Marques <edu.temarques@gmail.com>
2019-12-14 12:57:26 -0300

    feat: export material container

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { UiMenu } from "./components/UiMenu"
 export { UiNavbar } from "./components/UiNavbar"
 export { UiProductCard } from "./components/UiProductCard"
 export {
+  Container as UiContainer,
   Grid as UiGrid,
   Typography as UiTypography,
   withStyles as UiWithStyles,
