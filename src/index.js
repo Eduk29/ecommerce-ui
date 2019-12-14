bdc8e4826ecebd98d8195385423af43d8f679322
Eduardo Marques <edu.temarques@gmail.com>
2019-12-14 00:36:13 -0300

    feat: Adjust in material export alias

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 export { UiMenu } from "./components/UiMenu"
 export { UiNavbar } from "./components/UiNavbar"
 export { UiProductCard } from "./components/UiProductCard"
-export { UiGrid, UiTypography, UiWithStyles } from "@material-ui/core"
+export {
+  Grid as UiGrid,
+  Typography as UiTypography,
+  withStyles as UiWithStyles,
+} from "@material-ui/core"
